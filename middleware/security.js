0cabb93485cf64e348959c7bf0bf1f96660bec9e
Salah <Salah>
2024-10-31 19:57:42 +0100

    remove comments

--- middleware/security.js
+++ middleware/security.js
@@ -3,12 +3,6 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 
-// exports.securityMiddleware = [
-//   helmet(), // Set security HTTP headers
-//   mongoSanitize(), // Prevent NoSQL injection
-//   xss(), // Prevent XSS attacks
-// ];
-
 exports.applySecurityMiddleware = (app) => {
   app.use(helmet()); // Set security HTTP headers
   app.use(mongoSanitize()); // Prevent NoSQL injection
